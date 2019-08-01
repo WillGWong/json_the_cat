247b17bd03a544f9203cafea8b5a5b26eb44cb57
Willey Wong <willeyw@gmail.com>
2019-08-01 22:02:19 +0000

    added test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,21 @@
 const request = require('request');
-let breedSearch = 'https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2];
 
 
-request(breedSearch, (error, response, body) => {
-  if (error) {
-    return console.log("Failed to receive details", error);
-  }
+const fetchBreedDescription = function(breedSearch, callback) {
+  let breedName = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedSearch
+  request(breedName, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
 
-  let breedObject = JSON.parse(body);
+    let breedObject = JSON.parse(body);
 
-  if (breedObject[0]) {
-    console.log(breedObject[0]['description']);
-  } else {
-    console.log("Breed not found");
-  }
-});
+    if (breedObject[0]) {
+      callback(null, breedObject[0]['description']);
+    } else {
+      callback(error, "Breed not found");
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
